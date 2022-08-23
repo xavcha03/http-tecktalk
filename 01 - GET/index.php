fa7ebb12af0b9eebe6abd51ec74cff03bd8488b1
xavcha03 <ch.xavier03@gmail.com>
2022-08-23 23:05:42 +0200

    html sans php

--- 01 - GET/index.php
+++ 01 - GET/index.php
@@ -1,5 +1,13 @@
-<?php
-       
-    echo "Voici du contenu";
-    
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Page html sans php</h1>
+    <p>Voici une page web Sans php</p>
+</body>
+</html>
