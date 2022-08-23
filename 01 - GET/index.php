25fe2eea0099c7f76c65a611e1b61e534596ff7f
xavcha03 <ch.xavier03@gmail.com>
2022-08-23 23:01:46 +0200

    add php content in html

--- 01 - GET/index.php
+++ 01 - GET/index.php
@@ -1,5 +1,17 @@
 <?php
-       
-    echo "Voici du contenu";
-    
+    $title = "Hello Becode";
+    $content = "Voici un peu de contenu"
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo $title?></h1>
+    <p><?=$content?></p>
+</body>
+</html>
