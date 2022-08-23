bbe8b569f75453a1835377ede227cd75f0ceeb4c
xavcha03 <ch.xavier03@gmail.com>
2022-08-23 23:11:34 +0200

    forcer format text

--- 01 - GET/index.php
+++ 01 - GET/index.php
@@ -1,17 +1,8 @@
 <?php
-    $title = "Hello Becode";
-    $content = "Voici un peu de contenu"
+    header('Content-Type: text/plain');
+    echo "Forcer le format text";
+
+
+
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1><?php echo $title?></h1>
-    <p><?=$content?></p>
-</body>
-</html>
