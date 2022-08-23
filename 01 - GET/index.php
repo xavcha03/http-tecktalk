10dd4d103cac95b500102bcd773d4b8fe6b57f03
xavcha03 <ch.xavier03@gmail.com>
2022-08-23 23:23:30 +0200

    forcer contenu JSON en format text

--- 01 - GET/index.php
+++ 01 - GET/index.php
@@ -1,21 +1,10 @@
 <?php
     header('Content-Type: text/plain');
     echo "Forcer le format text";
-
+    $fruits = array("Pommes", "fraises", "Banane");
+    $fruitInJson = json_encode($fruit);
+    echo $fruit;
 
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Un bon titre HTML</h1>
-    
-</body>
-</html>
