a40893cf4f313d01368e7553d76c4a0097ba6692
xavcha03 <ch.xavier03@gmail.com>
2022-08-23 23:54:32 +0200

    Mini formulaire

---
+++ 01 - GET/api.php
@@ -0,0 +1,13 @@
+<?php
+//DEBUG DE LA VARIABLE GLOBALE
+//var_dump($_POST); // ~echo pour debug
+
+$firstName = $_POST["firstName"];
+
+echo $firstName;
+
+
+?>
+
+<br />
+<a href="index.php"><< retour</a>
--- 01 - GET/index.php
+++ 01 - GET/index.php
@@ -1,10 +1,19 @@
-<?php
-    header('Content-Type: text/plain');
-    echo "Forcer le format text";
-    $fruits = array("Pommes", "fraises", "Banane");
-    $fruitInJson = json_encode($fruit);
-    echo $fruit;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <h1>Formulaire simple</h1>
 
 
-
-?>
+     <form method="POST" action="api.php">
+        <input type="text" name="firstName" value="Xavier">
+        <button typ="submit">Envoyer</button> 
+    </form>
+     
+</body>
+</html>
